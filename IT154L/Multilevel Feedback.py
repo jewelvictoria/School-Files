33fb62393cc8f9e3893f20e6dedb113a9330ad95
jewelvictoria <55025198+jewelvictoria@users.noreply.github.com>
2020-09-19 20:23:40 +0800

    Add files via upload
    Adapted to Multilevel Queueing

--- IT154L/Multilevel Feedback.py
+++ IT154L/Multilevel Feedback.py
@@ -68,13 +68,14 @@ while(time < sum_b):
 		o = o + 1
 
 print("FCFS:",process_list)
+
 #SJF-P
 
 a = [0,2,4,5]
 b = [7,4,1,4]
 p = [4,3,2,1]
-w = [0,0,0,0,0]
-ta = [0,0,0,0,0]
+w = [0,0,0,0]
+ta = [0,0,0,0]
 
 sum_b = sum(b)
 time = 0
@@ -84,18 +85,19 @@ process_dict = dict()
 i = 0
 is_process_executed = 0
 
-while (i<size):
+while (i < size):
 	process_dict[i] = [a[i], b[i], p[i], w[i], ta[i], 0]
 	i = i + 1
 
-while(time < sum_b):
-	available_dict = []
+while(is_process_executed < size):
 
-	for i in range(size):
-		if(process_dict[i][0] <= time and process_dict[i][1] > 0):
-			available_dict.append(i)
+	available_dict = []
 	
+	for i in range(size):	
+		if(process_dict[i][0] <= time and process_dict[i][1] > 0 and process_dict[i][5] == 0): 
+			available_dict.append(i)
 	smallest = 999
+
 	for j in available_dict:
 		if(process_dict[j][1] < smallest and process_dict[j][1] > 0 ):
 			smallest = process_dict[j][1]
@@ -120,9 +122,16 @@ while(time < sum_b):
 		process_dict[smallest_index][1] = process_dict[smallest_index][1] - 1
 		time = time + 1
 
+		if(len(available_dict) == 1 and process_dict[smallest_index][1] == 0):
+			process_dict[smallest_index][5] = 1
+			is_process_executed = is_process_executed + 1
+			
+		elif(len(process_list) > 1):
+			if(process_list[-2] != process_list[-1]):
+				process_dict[process_list[-2]-1][5] = 1
+				is_process_executed = is_process_executed + 1
+
 		
-
-
 print("SJF-P:",process_list)
 
 #SJF-NP
@@ -211,9 +220,9 @@ print("SJF-NP:",process_list)
 
 #Priority Preemptive
 
-a = [0,2,4,5]
-b = [7,4,1,4]
-p = [4,3,2,1]
+a = [0,2,5,4]
+b = [7,4,5,4]
+p = [4,3,1,2]
 w = [0,0,0,0,0]
 ta = [0,0,0,0,0]
 
@@ -227,11 +236,13 @@ i = 0
 while (i<len(a)):
 	process_dict[i] = [a[i], b[i], p[i], w[i], ta[i], 0]
 	i = i + 1
-while(time < sum_b):
+
+is_process_executed = 0
+while(is_process_executed < size):
 	available_dict = []
 
 	for i in range(len(a)):
-		if(process_dict[i][0] <= time and process_dict[i][1] > 0):
+		if(process_dict[i][0] <= time and process_dict[i][1] > 0 and process_dict[i][5] == 0):
 			available_dict.append(i)
 	
 	smallest = 999
@@ -260,6 +271,14 @@ while(time < sum_b):
 		process_dict[smallest_index][1] = process_dict[smallest_index][1] - 1
 		time = time + 1
 
+		if(len(available_dict) == 1 and process_dict[smallest_index][1] == 0):
+			process_dict[smallest_index][5] = 1
+			is_process_executed = is_process_executed + 1
+			
+		elif(len(process_list) > 1):
+			if(process_list[-2] != process_list[-1]):
+				process_dict[process_list[-2]-1][5] = 1
+				is_process_executed = is_process_executed + 1
 
 print("P-P:",process_list)
 
@@ -313,6 +332,8 @@ while(time < sum_b):
 		process_dict[smallest_index][2] = 0
 		process_dict[smallest_index][1] = process_dict[smallest_index][1] - 1
 		time = time + 1
+
+		
 
 
 print("P-NP:",process_list)
